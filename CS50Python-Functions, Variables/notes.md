82ebe9a2f97fec954daa5c342c6435526b9fb1b1
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-03-26 23:38:08 -0600

    Examples List comprehension

--- CS50Python-Functions, Variables/notes.md
+++ CS50Python-Functions, Variables/notes.md
@@ -357,9 +357,14 @@ def print_square(size):
     for i in range(size):
         print("#" * size)
 
+def print_square(size):
+    for i in range(size):
+        print_row(size)
 
+def print_row(width):
+    print("#" * width)
 
-
+main()
 
 
 
---
+++ CS50Python-Loops/python-list-comprehension.py
@@ -0,0 +1,13 @@
+# Python List comprehension provides a much more short syntax 
+# for creating a new list based on the values of an existing list.
+
+#Advantages of List Comprehension
+"""
+ - More time-efficient and space-efficient than loops.
+ - Require fewer lines of code.
+ - Transforms iterative statement into a formula.
+ """
+# Syntax of  List Comprehension
+"""
+newList = [ expression(element) for element in oldList if condition ] 
+"""
